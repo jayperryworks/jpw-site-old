1186152dc1c77950a1cdf4a43945d4aaaff3565f
Jay Perry <hi@jayperryworks.com>
2014-05-31 19:27:56 -0400

    jekyll setup w/gulp tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -19,70 +19,99 @@ var path = {
 var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function () {
-    return gulp.src('app/' + path.css + '/main.scss')
-        .pipe($.rubySass({
-            style: 'expanded',
-            bundleExec: 'true',
-            lineNumbers: 'true',
-            loadPath: 'app/' + path.bower,
-            precision: 10
-        }))
-        .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('.tmp/' + path.css))
-        .pipe($.size());
+  return gulp.src('app/' + path.css + '/main.scss')
+    .pipe($.rubySass({
+      style: 'expanded',
+      bundleExec: 'true',
+      lineNumbers: 'true',
+      loadPath: 'app/' + path.bower,
+      precision: 10
+    }))
+    .pipe($.autoprefixer('last 1 version'))
+    .pipe(gulp.dest('.tmp/' + path.css))
+    .pipe($.size());
+});
+
+gulp.task('styles-serve', function () {
+  return gulp.src('app/' + path.css + '/main.scss')
+    .pipe($.rubySass({
+      style: 'expanded',
+      bundleExec: 'true',
+      lineNumbers: 'true',
+      loadPath: 'app/' + path.bower,
+      precision: 10
+    }))
+    .pipe($.autoprefixer('last 1 version'))
+    .pipe(gulp.dest('dist/' + path.css))
+    .pipe($.size());
 });
 
 gulp.task('scripts', function () {
-    return gulp.src('app/' + path.js + '/**/*.js')
-        .pipe($.jshint())
-        .pipe($.jshint.reporter(require('jshint-stylish')))
-        .pipe($.size());
+  return gulp.src('app/' + path.js + '/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter(require('jshint-stylish')))
+    .pipe($.size());
+});
+
+gulp.task('scripts-serve', function () {
+  return gulp.src('app/' + path.js + '/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter(require('jshint-stylish')))
+    .pipe(gulp.dest('dist/' + path.js))
+    .pipe($.size());
+});
+
+// Runs the build command for Jekyll to compile the site locally
+// Only used for local server -- for build, pull uncompiled files and send to github
+gulp.task('jekyll', function() {
+  return gulp.src('')
+    .pipe($.exec("jekyll build"));
 });
 
 gulp.task('html', ['styles', 'scripts'], function () {
-    var jsFilter = $.filter('**/*.js');
-    var cssFilter = $.filter('**/*.css');
+  var jsFilter = $.filter('**/*.js');
+  var cssFilter = $.filter('**/*.css');
 
-    return gulp.src('app/*.html')
-        .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
-        .pipe(jsFilter)
-        .pipe($.uglify())
-        .pipe(jsFilter.restore())
-        .pipe(cssFilter)
-        .pipe($.csso())
-        .pipe(cssFilter.restore())
-        .pipe($.useref.restore())
-        .pipe($.useref())
-        .pipe(gulp.dest('dist'))
-        .pipe($.size());
+  return gulp.src('app/**/*{.html,.md}')
+    .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
+    .pipe(jsFilter)
+    .pipe($.uglify())
+    .pipe(jsFilter.restore())
+    .pipe(cssFilter)
+    .pipe($.csso())
+    .pipe(cssFilter.restore())
+    .pipe($.useref.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest('dist'))
+    .pipe($.size());
 });
 
 gulp.task('images', function () {
-    return gulp.src('app/' + path.img + '/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/' + path.img))
-        .pipe($.size());
+  return gulp.src('app/' + path.img + '/**/*')
+    .pipe($.cache($.imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/' + path.img))
+    .pipe($.size());
 });
 
 gulp.task('fonts', function () {
-    return gulp.src('app/assets/fonts/**/*')
-        // .pipe($.filter('*.{eot,svg,ttf,woff}'))
-        // .pipe($.flatten())
-        .pipe(gulp.dest('dist/' + path.fonts))
-        .pipe($.size());
+  return gulp.src('app/assets/fonts/**/*')
+    // .pipe($.filter('*.{eot,svg,ttf,woff}'))
+    // .pipe($.flatten())
+    .pipe(gulp.dest('dist/' + path.fonts))
+    .pipe($.size());
 });
 
 gulp.task('extras', function () {
-    return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
-        .pipe(gulp.dest('dist'));
+  return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function () {
-    return gulp.src(['.tmp', 'dist/**/*', '!dist/{.git,.gitignore,CNAME,README.md,LICENSE}'], { read: false }).pipe($.clean());
+  return gulp.src(['.tmp', 'dist/**/*', '!dist/{.git,.gitignore,CNAME,README.md,LICENSE}'], { read: false }).pipe($.clean());
 });
 
 gulp.task('build', ['html', 'images', 'fonts', 'extras']);
@@ -92,57 +121,60 @@ gulp.task('default', ['clean'], function () {
 });
 
 gulp.task('connect', function () {
-    var connect = require('connect');
-    var app = connect()
-        .use(require('connect-livereload')({ port: 35729 }))
-        .use(connect.static('app'))
-        .use(connect.static('.tmp'))
-        .use(connect.directory('app'));
+  var connect = require('connect');
+  var app = connect()
+    .use(require('connect-livereload')({ port: 35729 }))
+    .use(connect.static('dist'))
+    .use(connect.static('.tmp'))
+    .use(connect.directory('dist'));
 
-    require('http').createServer(app)
-        .listen(9000)
-        .on('listening', function () {
-            console.log('Started connect web server on http://localhost:9000');
-        });
+  require('http').createServer(app)
+    .listen(9000)
+    .on('listening', function () {
+        console.log('Started connect web server on http://localhost:9000');
+    });
 });
 
-gulp.task('serve', ['connect', 'styles'], function () {
+gulp.task('serve', ['connect', 'styles-serve'], function () {
     require('opn')('http://localhost:9000');
 });
 
 // inject bower components
 gulp.task('wiredep', function () {
-    var wiredep = require('wiredep').stream;
+  var wiredep = require('wiredep').stream;
 
-    gulp.src('app/' + path.css + '/*.scss')
-        .pipe(wiredep({
-            directory: 'app/' + path.bower
-        }))
-        .pipe(gulp.dest('app/styles'));
+  gulp.src('app/' + path.css + '/*.scss')
+    .pipe(wiredep({
+        directory: 'app/' + path.bower
+    }))
+    .pipe(gulp.dest('app/styles'));
 
-    gulp.src('app/*.html')
-        .pipe(wiredep({
-            directory: 'app/' + path.bower
-        }))
-        .pipe(gulp.dest('app'));
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+        directory: 'app/' + path.bower
+    }))
+    .pipe(gulp.dest('app'));
 });
 
 gulp.task('watch', ['connect', 'serve'], function () {
-    var server = $.livereload();
+  var server = $.livereload();
 
-    // watch for changes
+  // watch for changes
 
-    gulp.watch([
-        'app/*.html',
-        '.tmp/' + path.css + '/**/*.css',
-        'app/' + path.js + '/**/*.js',
-        'app/' + path.img + '/**/*'
-    ]).on('change', function (file) {
-        server.changed(file.path);
-    });
+  gulp.watch([
+    'dist/**/*.html',
+    '{.tmp,dist}/' + path.css + '/**/*.css',
+    'dist/' + path.js + '/**/*.js',
+    'dist/' + path.img + '/**/*'
+  ]).on('change', function (file) {
+    server.changed(file.path);
+  });
 
-    gulp.watch('app/' + path.css + '/**/*.scss', ['styles']);
-    gulp.watch('app/' + path.js + '/**/*.js', ['scripts']);
-    gulp.watch('app/' + path.img + '/**/*', ['images']);
-    gulp.watch('bower.json', ['wiredep']);
+  gulp.watch('app/**/*.html', ['jekyll']);
+  gulp.watch('app/**/*.md', ['jekyll']);
+  gulp.watch('**/*.yml', ['jekyll']);
+  gulp.watch('app/' + path.css + '/**/*.scss', ['styles-serve']);
+  gulp.watch('app/' + path.js + '/**/*.js', ['scripts-serve']);
+  gulp.watch('app/' + path.img + '/**/*', ['images']);
+  gulp.watch('bower.json', ['wiredep']);
 });
